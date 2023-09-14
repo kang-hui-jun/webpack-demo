5ac4226ac015d534895e76a75feeb009b227ee97
k hj <1470166217@qq.com>
2023-09-14 15:41:57 +0800

    feat: 首屏loading加载

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,14 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CompressionPlugin = require("compression-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-  plugins: [new CompressionPlugin()],
+  plugins: [
+    // new CompressionPlugin(),
+    new BundleAnalyzerPlugin(),
+  ],
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function App() {
-  console.log(123);
-
   return <div>App</div>;
 }
